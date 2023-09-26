194a43b112f0c40f7850e245ba6660995957bf8c
Adrian reyes <adriandmc05@gmail.com>
2023-09-25 22:21:10 -0400

    update to scss

---
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss'],
+})
+export class NavBarComponent implements OnInit {
+  items: MenuItem[] | undefined;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'File',
+        icon: 'pi pi-fw pi-file',
+      },
+      {
+        label: 'Edit',
+        icon: 'pi pi-fw pi-pencil',
+      },
+      {
+        label: 'Users',
+        icon: 'pi pi-fw pi-user',
+        items: [
+          {
+            label: 'New',
+            icon: 'pi pi-fw pi-user-plus',
+          },
+          {
+            label: 'Delete',
+            icon: 'pi pi-fw pi-user-minus',
+          },
+          {
+            label: 'Search',
+            icon: 'pi pi-fw pi-users',
+            items: [
+              {
+                label: 'Filter',
+                icon: 'pi pi-fw pi-filter',
+                items: [
+                  {
+                    label: 'Print',
+                    icon: 'pi pi-fw pi-print',
+                  },
+                ],
+              },
+              {
+                icon: 'pi pi-fw pi-bars',
+                label: 'List',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        label: 'Events',
+        icon: 'pi pi-fw pi-calendar',
+      },
+      {
+        label: 'Quit',
+        icon: 'pi pi-fw pi-power-off',
+      },
+    ];
+  }
+}
